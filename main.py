3e76bc5dca806efde50627dc150abeea55861a4f
Muhammad Farhan <muhammad.farhan@bfi.co.id>
2023-03-10 10:21:44 +0700

    updated main.py with comment

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# import the module/class
 from gpt_usecase import UseCaseGPT
 
 if __name__ == '__main__':
 
+    # define the UseCaseGPT()
     usecase = UseCaseGPT()
+    # pass your code path you want to be explained as the argument in the file_path parameter
     result = usecase.code_explainer(file_path="farhan_gpt.py")
+    # the explanation will be returned as a string
     print(result)
