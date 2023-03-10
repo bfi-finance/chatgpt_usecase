8aa0438015c48a024c3a0f05826feb5206739739
Muhammad Farhan <muhammad.farhan@bfi.co.id>
2023-03-10 09:59:00 +0700

    main.py: just cleaned the code

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-# from farhan_gpt import MyGPT
 from gpt_usecase import UseCaseGPT
 
 if __name__ == '__main__':
-    # gpt = MyGPT()
-    #
-    # input_text = """
-    # cukup jawab dengan kata negatif/positif/netral
-    #
-    # apa sentimen dari kalimat di bawah ini:
-    #
-    # "Sy ngeliat bokep krn sudah muak dgn kehidupan"
-    # """
-    # response = gpt.input_text(text=input_text)
-    # print(response)
-
-    # text = """
-    # Lagian pake sok asik bagi-bagi tiket konser K-Pop
-    # padahal biar elektabilitas naik udah bener perbanyak
-    # foto kucing Menhan aja wqwqwqwqwqwqwqwqwqwqqwqwqwqwqwqqwqqwqwqwq
-    # """
 
     usecase = UseCaseGPT()
     result = usecase.code_explainer(file_path="farhan_gpt.py")
